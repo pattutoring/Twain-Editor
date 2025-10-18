71fb08b3b8cce3a9c64e526d77fcc422a459d84c
thejokerphd <77590908+thejokerphd@users.noreply.github.com>
2025-10-18 19:45:52 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import TwainStudentEditor from "./TwainStudentEditor.js";
+
+ReactDOM.render(
+  React.createElement(TwainStudentEditor, {
+    title: "PAT Tutoring — Twain Writing Practice",
+  }),
+  document.getElementById("root")
+);
