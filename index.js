9090ffff4b36f74b1eed8f87723fa980b43d4770
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-18 20:29:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 import TwainStudentEditor from "./TwainStudentEditor.js";
 
-
-
-
-
-
+// Render the Twain Editor in the container div
 ReactDOM.render(
-  React.createElement(TwainStudentEditor, {
-    title: "PAT Tutoring — Twain Writing Practice",
-  }),
-  document.getElementById("root")
+  React.createElement(TwainStudentEditor, { title: "Your Twain Editor" }),
+  document.getElementById("editor-container")
 );
