f3f1a78b04d87f8f1312f8ad8122968c25745cbc
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-18 20:37:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 import TwainStudentEditor from "./TwainStudentEditor.js";
 
-// Render the Twain Editor in the container div
+// Render the Twain Editor
 ReactDOM.render(
   React.createElement(TwainStudentEditor, { title: "Your Twain Editor" }),
   document.getElementById("editor-container")
